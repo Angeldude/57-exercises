b3654384f5c8703aba9146534b01d36a0f763bfa
Angel Vanegas <angelman@gmail.com>
2017-11-25 16:55:48 -0500

    figure out how to get input without storing in variable

--- hallo.hs
+++ hallo.hs
@@ -1,5 +1,5 @@
 module Main where
-    main = do 
-        putStr "What is your name? "
-        name <- getLine
-        putStrLn ("Hi, " ++ name ++ ", a pleasure to meet you.")
+main = do 
+  putStr "What is your name? "
+  name <- getLine
+  putStrLn ("Hi, " ++ name ++ ", a pleasure to meet you.")
